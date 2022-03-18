1d6fa99cbb166d618f3cfbd9f434a21168c0ea6c
Peter Ulf Jørgensen <peter@puj.dk>
2022-03-18 13:21:07 +0100

    Gamle filer slettet. Nye omdøbt

--- javaScript.js
+++ javaScript.js
@@ -1,179 +1,332 @@
-let myHeaders = {
-  "x-apikey": "6225f2a2dced170e8c839fef	",
+const key = "6225f2a2dced170e8c839fef";
+const getAllUrl = "https://keawords-3374.restdb.io/rest/keawords";
+const putHeaders = {
+  "Content-Type": "application/json",
+  "x-apikey": key,
 };
+
+const getHeaders = {
+  "x-apikey": key,
+};
+const searchWord = document.querySelector("#search_word");
+const create = document.querySelector("#search_button");
+create.addEventListener("click", createPost);
+
+let results4;
+let background = "odd";
 
 document.addEventListener("DOMContentLoaded", loadJSON);
 
-// let JSONContent;
-
 async function loadJSON() {
-  const JSONData = await fetch(
-    "https://keawords-3374.restdb.io/rest/keawords  ",
-    {
-      headers: myHeaders,
-    }
-  );
+  const JSONData = await fetch(getAllUrl, {
+    headers: getHeaders,
+  });
   const JSONContent = await JSONData.json();
+  if (JSONContent != "") {
+    document.querySelector(".preloader").classList.add("hide");
+    searchWord.focus();
+  }
 
-  // showContent(JSONContent);
-  console.log("JSONContent", JSONContent);
+  seachResult(searchWord, JSONContent);
 
-  autocomplete(document.getElementById("myInput"), JSONContent);
-  document.getElementById("myInput").focus();
+  searchWord.addEventListener("keyup", function () {
+    seachResult(searchWord, JSONContent);
+  });
 }
 
-function autocomplete(inp, arr) {
-  /*the autocomplete function takes two arguments,
-  the text field element and an array of possible autocompleted values:*/
-  console.log(arr);
-  console.log(inp);
+function seachResult(word, arr) {
+  console.log("seachResult");
+  // tøm content før det rettes til
 
-  var currentFocus;
-  /*execute a function when someone writes in the text field:*/
-  inp.addEventListener("input", function (e) {
-    var a,
-      b,
-      i,
-      val = this.value;
-    /*close any already open lists of autocompleted values*/
-    closeAllLists();
-    if (!val) {
-      return false;
+  document.querySelector("#content").textContent = "";
+
+  //filter array her
+  let results1;
+  results1 = arr.filter((singleWord) => {
+    if (word.value != "") {
+      // singleWord.word.toLowerCase() == word.value.toLowerCase();
+      console.log("singleWord.word", singleWord.word);
+      return singleWord.word.toLowerCase().includes(word.value.toLowerCase());
+    } else {
+      return true;
     }
-    currentFocus = -1;
-    /*create a DIV element that will contain the items (values):*/
-    a = document.createElement("DIV");
-    a.setAttribute("id", this.id + "autocomplete-list");
-    a.setAttribute("class", "autocomplete-items");
-    /*append the DIV element as a child of the autocomplete container:*/
-    this.parentNode.appendChild(a);
-    /*for each item in the array...*/
-    for (i = 0; i < arr.length; i++) {
-      /*check if the item starts with the same letters as the text field value:*/
-      if (
-        arr[i].word.substr(0, val.length).toUpperCase() == val.toUpperCase()
-      ) {
-        /*create a DIV element for each matching element:*/
-        b = document.createElement("DIV");
-        /*make the matching letters bold:*/
-        b.innerHTML =
-          "<strong>" + arr[i].word.substr(0, val.length) + "</strong>";
-        b.innerHTML += arr[i].word.substr(val.length);
-        /*insert a input field that will hold the current array item's value:*/
-        b.innerHTML += "<input type='hidden' value='" + arr[i].word + "'>";
-        b.setAttribute("data-id", arr[i]._id);
-        /*execute a function when someone clicks on the item value (DIV element):*/
-        b.addEventListener("click", function (e) {
-          /*insert the value for the autocomplete text field:*/
-          inp.value = this.getElementsByTagName("input")[0].value;
-          /*close the list of autocompleted values,
-              (or any other open lists of autocompleted values:*/
-          closeAllLists();
-        });
-        a.appendChild(b);
+  });
+  console.log("results1", results1);
+  let results2 = arr.filter((singleWord) => {
+    //hvis søgeboxen for tema IKKE er tom
+    if (searchWord.value != "") {
+      //console.log("temasøg er ikke tom");
+      //console.log(singleWord.themes);
+      //her bruges "some" til at tjekke om søge temaet er en del af der der står i databasne
+      //jeg forstår ikke helt nedenstående, men det bruges sammen med some
+      //jeg bruger det til at tjekke hvert element og om det der er skrevet i søgeboksen er en del af det ord der er i arrayet
+      const likeInputTheme = (element) =>
+        element.toLowerCase().includes(word.value.toLowerCase());
+
+      //hvis nedenstående er sandt kommer den med i filteret
+      if (typeof singleWord.themes == "string") {
+        return singleWord.themes.includes(word.value.toLowerCase());
+      } else {
+        return singleWord.themes.some(likeInputTheme);
       }
+    } else {
+      console.log("temasøg er tom");
+      //hvis søgeboxen for tema ER tom
+      return true;
+    }
+  });
+  console.log("results2", results2);
+
+  let results3 = arr.filter((singleWord) => {
+    //hvis søgeboxen for beskrivelse IKKE er tom
+    if (word.value != "") {
+      console.log("søgefeltet beskr er IKKE tomt");
+      //hvis nedenstående er sandt kommer den med i filteret
+      // return singleWord.explanation
+      //   .toLowerCase()
+      //   .includes(word.value.toLowerCase());
+      return;
+    } else {
+      //hvis søgeboxen for beskrivelse ER tom
+      console.log("søgefeltet beskr er  tomt");
+      return true;
     }
   });
 
-  /*execute a function presses a key on the keyboard:*/
-  inp.addEventListener("keydown", function (e) {
-    var x = document.getElementById(this.id + "autocomplete-list");
-    if (x) x = x.getElementsByTagName("div");
-    if (e.keyCode == 40) {
-      /*If the arrow DOWN key is pressed,
-        increase the currentFocus variable:*/
-      currentFocus++;
-      /*and and make the current item more visible:*/
-      addActive(x);
-    } else if (e.keyCode == 38) {
-      //up
-      /*If the arrow UP key is pressed,
-        decrease the currentFocus variable:*/
-      currentFocus--;
-      /*and and make the current item more visible:*/
-      addActive(x);
-    } else if (e.keyCode == 13) {
-      /*If the ENTER key is pressed, prevent the form from being submitted,*/
-      e.preventDefault();
-      if (currentFocus > -1) {
-        /*and simulate a click on the "active" item:*/
-        if (x) x[currentFocus].click();
-      }
+  console.log("results3", results3);
+
+  let results45 = results1.concat(results2.concat(results3));
+
+  //fjern dubletter
+  results4 = results45.filter(
+    (item, index) => results45.indexOf(item) === index
+  );
+  results4.sort(compareWord);
+  results4.sort(compareThemes);
+
+  //sort arrayet: først på theme og derefter på word
+  function compareThemes(a, b) {
+    if (a.themes < b.themes) {
+      return -1;
     }
+    if (a.themes > b.themes) {
+      return 1;
+    }
+    return 0;
+  }
+  function compareWord(a, b) {
+    if (a.word < b.word) {
+      return -1;
+    }
+    if (a.word > b.word) {
+      return 1;
+    }
+    return 0;
+  }
+  console.log("results4", results4);
+
+  // document.querySelector("#content").textContent = "";
+
+  // results4.forEach((result) => {
+  generateListItem();
+  // });
+}
+function generateEditItem(result, id) {}
+
+function generateListItem(id) {
+  document.querySelector("#content").textContent = "";
+  results4.forEach((result) => {
+    const template = document.querySelector("#my_result");
+
+    const clone = template.content.cloneNode(true);
+    if (background == "even") {
+      background = "odd";
+    } else {
+      background = "even";
+    }
+
+    //console.log(result.word);
+    clone.querySelector(".search_word").textContent = result.word;
+    clone.querySelector(".search_word").classList.add(background);
+
+    clone.querySelector(".search_theme").textContent = result.themes;
+    clone.querySelector(".search_theme").classList.add(background);
+
+    clone.querySelector(".search_description").textContent = result.explanation;
+    clone.querySelector(".search_description").classList.add(background);
+
+    clone.querySelector(".edit_button").classList.add(background);
+    //console.log("id: ", id);
+
+    clone.querySelector(".edit_button").addEventListener("click", () => {
+      edit_form(result._id, results4);
+    });
+    document.querySelector("#content").appendChild(clone);
   });
-  function addActive(x) {
-    /*a function to classify an item as "active":*/
-    if (!x) return false;
-    /*start by removing the "active" class on all items:*/
-    removeActive(x);
-    if (currentFocus >= x.length) currentFocus = 0;
-    if (currentFocus < 0) currentFocus = x.length - 1;
-    /*add class "autocomplete-active":*/
-    x[currentFocus].classList.add("autocomplete-active");
-  }
-  function removeActive(x) {
-    /*a function to remove the "active" class from all autocomplete items:*/
-    for (var i = 0; i < x.length; i++) {
-      x[i].classList.remove("autocomplete-active");
-    }
-  }
-  function closeAllLists(elmnt) {
-    /*close all autocomplete lists in the document,
-    except the one passed as an argument:*/
-    var x = document.getElementsByClassName("autocomplete-items");
-    for (var i = 0; i < x.length; i++) {
-      if (elmnt != x[i] && elmnt != inp) {
-        x[i].parentNode.removeChild(x[i]);
+}
+
+function edit_form(id, arr) {
+  document.querySelector("#content").textContent = "";
+
+  const mySearchResult = arr.find((x) => x._id === id);
+  const template = document.querySelector("#my_edit");
+  const clone = template.content.cloneNode(true);
+  clone.querySelector(".search_word").value = mySearchResult.word;
+  clone.querySelector(".search_theme").value = mySearchResult.themes;
+  clone.querySelector(".search_description").value = mySearchResult.explanation;
+  // clone.querySelector(".edit_button .save").textContent = "gem";
+  clone.querySelector(".edit_button .save").addEventListener("click", () => {
+    update(mySearchResult._id);
+  });
+  // clone.querySelector(".edit_button .delete").textContent = "Slet";
+  clone.querySelector(".edit_button .delete").addEventListener("click", () => {
+    deleteWord(mySearchResult._id);
+  });
+  clone.querySelector(".edit_button .cancel").addEventListener("click", () => {
+    generateListItem();
+  });
+
+  document.querySelector("#content").appendChild(clone);
+
+  document.querySelector("#content .search_word").focus();
+}
+
+function update(id) {
+  // data to be sent to the POST request
+  document.querySelector(".preloader").classList.remove("hide");
+
+  const myUrl = `https://keawords-3374.restdb.io/rest/keawords/${id}`;
+  console.log(myUrl);
+
+  const update = {
+    word: document.querySelector("#content .search_word").value,
+    themes: document.querySelector("#content .search_theme").value.split(","),
+    explanation: document.querySelector("#content .search_description").value,
+  };
+
+  console.log("update", update);
+
+  const options = {
+    method: "PUT",
+    headers: putHeaders,
+    body: JSON.stringify(update),
+  };
+  fetch(myUrl, options)
+    .then((data) => {
+      if (!data.ok) {
+        throw Error(data.status);
       }
-    }
-    if (elmnt) {
-      //  explanation(elmnt.dataset.id, arr);
-      explanation(elmnt.innerText, arr);
-    }
-  }
-  /*execute a function when someone clicks in the document:*/
-  // document.addEventListener("click", function (e) {
-  document
-    // .querySelector("#myInputautocomplete-list")
-    .addEventListener("click", function (e) {
-      console.log("document klikket på");
-      closeAllLists(e.target);
+
+      //opdaterer listen når vi har rettet
+      // results4 = "";
+      document.querySelector("#content").textContent = "";
+      // document.querySelector(".search_word").textContent = "";
+
+      loadJSON();
+      generateListItem();
+
+      return data.json();
+    })
+    .then((update) => {
+      console.log(update);
+    })
+    .catch((e) => {
+      console.log(e);
     });
 }
 
-function explanation(searchWord, words) {
-  console.log("searchWord er ", searchWord);
+function createPost() {
+  document.querySelector("#content").textContent = "";
 
-  const results = words.filter((singleWord) => singleWord.word == searchWord);
-  //reset content
-  document.querySelector("#myTheme").value = "";
-  document.querySelector("#myContent").textContent = "";
-  console.log(results);
-
-  results.forEach((result) => {
-    var template = document.querySelector("#wordRow");
-    var clone = template.content.cloneNode(true);
-
-    clone.querySelector(".myInputRslt").textContent = result.word;
-
-    //let theme = clone.querySelector(".myThemeRslt").textContent;
-    //theme = result.word;
-    clone.querySelector(".myExplainRslt").textContent = result.explanation;
-
-    if (Array.isArray(result.themes) && result.themes.length > 1) {
-      result.themes.forEach((theme) => {
-        //document.querySelector("#myTheme").value += theme + ", ";
-
-        console.log("theme: ", theme);
-        clone.querySelector(".myThemeRslt").innerHTML += theme + "<br> ";
-      });
-    } else {
-      //document.querySelector("#myTheme").value = result[0].themes;
-      console.log("theme: ", result.themes[0]);
-      clone.querySelector(".myThemeRslt").textContent = result.themes[0];
-    }
-
-    //document.querySelector("#myExplain").value = result[0].explanation;
-    document.querySelector("#myContent").appendChild(clone);
+  const template = document.querySelector("#my_edit");
+  const clone = template.content.cloneNode(true);
+  clone.querySelector(".search_word").value = "";
+  clone.querySelector(".search_theme").value = "";
+  clone.querySelector(".search_description").value = "";
+  // clone.querySelector(".edit_button .save").textContent = "gem";
+  clone.querySelector(".edit_button .save").addEventListener("click", () => {
+    post();
   });
+
+  document.querySelector("#content").appendChild(clone);
+
+  document.querySelector("#content .search_word").focus();
+}
+
+function post() {
+  // data to be sent to the POST request
+
+  const myUrl = `https://keawords-3374.restdb.io/rest/keawords`;
+  console.log(myUrl);
+
+  const update = {
+    word: document.querySelector("#content .search_word").value,
+    themes: document.querySelector("#content .search_theme").value.split(","),
+    explanation: document.querySelector("#content .search_description").value,
+  };
+
+  console.log("update", update);
+
+  const options = {
+    method: "POST",
+    headers: putHeaders,
+    body: JSON.stringify(update),
+  };
+  fetch(myUrl, options)
+    .then((data) => {
+      if (!data.ok) {
+        throw Error(data.status);
+      }
+
+      //opdaterer listen når vi har rettet
+      // results4 = "";
+      document.querySelector("#content").textContent = "";
+      // document.querySelector(".search_word").textContent = "";
+
+      loadJSON();
+      generateListItem();
+
+      return data.json();
+    })
+    .then((update) => {
+      console.log(update);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+}
+
+function deleteWord(id) {
+  // data to be sent to the POST request
+  document.querySelector(".preloader").classList.remove("hide");
+
+  const myUrl = `https://keawords-3374.restdb.io/rest/keawords/${id}`;
+  console.log(myUrl);
+
+  const options = {
+    method: "DELETE",
+    headers: putHeaders,
+  };
+  fetch(myUrl, options)
+    .then((data) => {
+      if (!data.ok) {
+        throw Error(data.status);
+      }
+
+      //opdaterer listen når vi har rettet
+      // results4 = "";
+      document.querySelector("#content").textContent = "";
+      // document.querySelector(".search_word").textContent = "";
+
+      loadJSON();
+      generateListItem();
+
+      return data.json();
+    })
+    .then((update) => {
+      console.log(update);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 }
